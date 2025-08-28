f65ad8f59ae8a071dd2447c473bd1e372a55543e
Jolly Good <1671375+good-lly@users.noreply.github.com>
2025-08-28 17:30:49 +0200

    update modules and fix url in index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Hono } from 'hono';
-import { studio } from '@outerbase/browsable-durable-object';
+import { Hono } from "hono";
+import { studio } from "@outerbase/browsable-durable-object";
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/yjs/snapshot/:roomId', async (c) => {
-	const roomId = c.req.param('roomId');
+app.get("/yjs/snapshot/:roomId", async (c) => {
+	const roomId = c.req.param("roomId");
 
 	const id = c.env.DURABLE_YJSPROVIDER.idFromName(roomId);
 	const stub = c.env.DURABLE_YJSPROVIDER.get(id);
@@ -13,26 +13,28 @@ app.get('/yjs/snapshot/:roomId', async (c) => {
 	return new Response(snapshot, {
 		status: 200,
 		headers: {
-			'Content-Type': 'application/octet-stream',
-			'Cache-Control': 'no-store', // Prevent caching of the snapshot
+			"Content-Type": "application/octet-stream",
+			"Cache-Control": "no-store", // Prevent caching of the snapshot
 		},
 	});
 });
 
-app.get('/yjs/ws/:roomId', (c) => {
-	const roomId = c.req.param('roomId');
+app.get("/yjs/ws/:roomId", (c) => {
+	const roomId = c.req.param("roomId");
 
 	const id = c.env.DURABLE_YJSPROVIDER.idFromName(roomId);
 	const stub = c.env.DURABLE_YJSPROVIDER.get(id);
 
-	const doUrl = new URL('https://example.com/ws');
+	const doUrl = new URL(
+		"wss://yjs-provider-test-1.jensenwtfwtf.workers.dev/yjs/ws",
+	);
 
 	const req = new Request(doUrl, { headers: c.req.raw.headers });
 	return stub.fetch(req);
 });
 
 if (import.meta.env.DEV) {
-	app.all('/studio', (c) => {
+	app.all("/studio", (c) => {
 		return studio(c.req.raw, c.env.DURABLE_YJSPROVIDER);
 	});
 }
@@ -43,4 +45,4 @@ export default {
 	},
 } satisfies ExportedHandler<Env>;
 
-export { YjsProvider } from './durable/YjsProvider';
+export { YjsProvider } from "./durable/YjsProvider";
